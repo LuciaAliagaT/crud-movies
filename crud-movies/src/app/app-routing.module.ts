44aff476d22a411c92e99689d21ce2147233f07d
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-22 21:38:53 -0500

    feature: changed styles

--- crud-movies/src/app/app-routing.module.ts
+++ crud-movies/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesService } from './services/movies.service';
 import { MoviesTableComponent } from './components/movies-table/movies-table.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: "home", component: MoviesTableComponent }
-  
+  { path: "home", component: HomeComponent },
+  { path: "movies", component: MoviesTableComponent }
 ];
 
 @NgModule({

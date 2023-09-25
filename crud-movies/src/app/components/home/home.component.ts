6a5b6888c90e783e2999e91df987f23e883c17bd
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-25 04:56:02 -0500

    feat: added add/edit card

--- crud-movies/src/app/components/home/home.component.ts
+++ crud-movies/src/app/components/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  pictures = [
+    { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793346455928842/4.png'},
+    { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793347370291230/5.png'},
+    { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793347840061501/1.png'},
+    { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793348083318855/2.png'},
+    { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793346149756969/3.png'}
+  ];
+  
 
+ 
+  
 }
--- crud-movies/src/app/components/movies-table/movies-table.component.ts
+++ crud-movies/src/app/components/movies-table/movies-table.component.ts
@@ -7,7 +7,6 @@ import { NgForm } from '@angular/forms';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Movie } from 'src/app/models/movie.model';
 
-
 @Component({
   selector: 'app-movies-table',
   templateUrl: './movies-table.component.html',
@@ -16,12 +15,11 @@ import { Movie } from 'src/app/models/movie.model';
 export class MoviesTableComponent {
   genres: string[] = ['Acción', 'Aventura', 'Ciencia ficción', 'Comedia', 'Drama', 'Fantasía', 'Crimen'];
   selectedGenre = '';
-
+  isShowCard = false;
   movieData!: Movie;
 
   @ViewChild('movieForm', {static: false})
   movieForm!: NgForm;
-
   displayedColumns = ['id', 'name', 'photo', 'length', 'genre', 'actions'];
   dataSource = new MatTableDataSource();
 
@@ -31,32 +29,9 @@ export class MoviesTableComponent {
   @ViewChild(MatSort, {static: true})
     sort!: MatSort;
 
-
-    onSubmit(): void {
-    if (this.movieForm.form.valid) {
-        console.log('Valid');
-        if (this.isEditMode) {
-            console.log('update');
-            this.updateMovie();
-        } else {
-          console.log('create');
-          this.addMovie();
-        }        
-        this.cancelEdit();
-    }else {
-      console.log('Valid data');
-    }
-    
-  }
   constructor(private moviesService: MoviesService, private router: Router) { 
     this.movieData = {} as Movie;
   }
-  cancelEdit(): void {
-    this.isEditMode = false;
-    this.movieData = {} as Movie; // Restablecer los datos del formulario
-    this.movieForm.reset(); // Restablecer el estado del formulario
-  }
-  
 
   ngOnInit(){
     this.dataSource.paginator = this.paginator;
@@ -81,9 +56,54 @@ export class MoviesTableComponent {
     }
   }
 
+  showCard(){
+    this.scrollToTop();
+    this.isShowCard = true;
+  }
+
+  cancelEdit(): void {
+    this.isEditMode = false;
+    this.isShowCard = false;
+    this.movieData = {} as Movie; // Restablecer los datos del formulario
+    this.movieForm.reset(); // Restablecer el estado del formulario
+  }
+
+  cancelAdd(): void {
+    this.isShowCard = false;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onSubmit(): void {
+    if (this.movieForm.form.valid) {
+        console.log('Valid');
+        if (this.isEditMode) {
+            console.log('update');
+            this.updateMovie();
+        } else {
+          console.log('create');
+          this.addMovie();
+        }        
+        this.cancelEdit();
+    }else {
+      console.log('Valid data');
+    }
+    
+  }
+
+  //CRUD
+
   editMovie(element: any): void{
-    this.movieData = element;
+    this.showCard();
     this.isEditMode = true;
+    this.movieData = element;
   }
 
   deleteMovie(id: string): void{
@@ -116,17 +136,13 @@ export class MoviesTableComponent {
     })
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
 
   getRow( row:any ){
     this.router.navigateByUrl(`/detail/${row.id}`);
+  }
+
+  scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   }
 
 }

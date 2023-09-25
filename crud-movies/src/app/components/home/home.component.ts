c8dad820252a0dfde947d4ec6061d3a3e4d7be0b
Jenn Espinoza <jenniespinoza2002@hotmail.com>
2023-09-25 12:54:51 -0500

    feat: implement caroulsel

--- crud-movies/src/app/components/home/home.component.ts
+++ crud-movies/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,15 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  pictures = [
+  currentIndex = 0;
+
+  images = [
     { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793346455928842/4.png'},
     { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793347370291230/5.png'},
     { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793347840061501/1.png'},
     { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793348083318855/2.png'},
     { src: 'https://media.discordapp.net/attachments/1154210086512758835/1155793346149756969/3.png'}
   ];
-  
 
- 
-  
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  nextSlide() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    }
+  }
 }
